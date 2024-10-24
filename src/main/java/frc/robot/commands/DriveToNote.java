5e9b9257c3375e7b7bd2f0cc7d27b9940a062c13
Logan Dhillon <logandhi64@gmail.com>
2024-10-24 16:07:12 -0400

    Call IntakeNote on DriveToNote#end

--- src/main/java/frc/robot/commands/DriveToNote.java
+++ src/main/java/frc/robot/commands/DriveToNote.java
@@ -7,20 +7,30 @@ import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.Constants.kDrive;
 import frc.robot.Constants.kVision;
 import frc.robot.subsystems.Drivetrain;
+import frc.robot.subsystems.Indexer;
+import frc.robot.subsystems.Intake;
 import frc.robot.subsystems.Vision;
 
 /**
  * @author Logan Dhillon
  */
 public class DriveToNote extends Command {
-    private final Vision vision;
-    private final Drivetrain drivetrain;
+    private final Vision sys_vision;
+    private final Drivetrain sys_drivetrain;
+    private final Intake sys_intake;
+    private final Indexer sys_indexer;
 
-    public DriveToNote() {
-        vision = Vision.getInstance();
-        drivetrain = Drivetrain.getInstance();
+    private final Command cmd_intake;
 
-        addRequirements(vision, drivetrain);
+    public DriveToNote(Vision vision, Drivetrain drivetrain, Intake intake, Indexer indexer) {
+        sys_vision = vision;
+        sys_drivetrain = drivetrain;
+        sys_intake = intake;
+        sys_indexer = indexer;
+
+        cmd_intake = new IntakeNote(sys_intake, sys_indexer);
+
+        addRequirements(sys_vision, sys_drivetrain, sys_intake, sys_indexer);
     }
 
     // Called when the command is initially scheduled.
@@ -32,21 +42,21 @@ public class DriveToNote extends Command {
     // Called every time the scheduler runs while the command is scheduled.
     @Override
     public void execute() {
-        drivetrain.moveWithVelocity(() -> vision.getTargetXOffset(), () -> vision.getTargetYOffset(),
+        sys_drivetrain.moveWithVelocity(() -> sys_vision.getTargetXOffset(), () -> sys_vision.getTargetYOffset(),
                 () -> kDrive.MAX_TURN_ANGULAR_VELOCITY);
     }
 
     // Called once the command ends or is interrupted.
     @Override
     public void end(boolean interrupted) {
-        
+        cmd_intake.schedule();
     }
 
     // Returns true when the command should end.
     @Override
     public boolean isFinished() {
-        double dx = vision.getTargetXOffset();
-        double dy = vision.getTargetYOffset();
+        double dx = sys_vision.getTargetXOffset();
+        double dy = sys_vision.getTargetYOffset();
         return (dx <= kVision.LOCATOR_TOLERANCE || dx >= -kVision.LOCATOR_TOLERANCE)
                 && (dy <= kVision.LOCATOR_TOLERANCE || dy >= -kVision.LOCATOR_TOLERANCE);
     }
