27e341df727bb486f455402b1095858597bd3b0d
Logan Dhillon <logandhi64@gmail.com>
2024-10-24 15:16:59 -0400

    Map joystick to drivetrain (teleop)

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -7,9 +7,15 @@ package frc.robot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import frc.robot.Constants.OperatorConstants;
+import frc.robot.Constants.kDrive;
 import frc.robot.commands.DriveToNote;
+import frc.robot.subsystems.Drivetrain;
 
 public class RobotContainer {
+    private final Drivetrain sys_drivetrain = Drivetrain.getInstance();
+
+    private Command cmd_teleopDrive;
+
     private final CommandXboxController m_driverController = new CommandXboxController(
             OperatorConstants.kDriverControllerPort);
 
@@ -24,6 +30,15 @@ public class RobotContainer {
      * Controller -> command bindings
      */
     private void configureBindings() {
+        // drive with joystick
+        cmd_teleopDrive = sys_drivetrain.moveWithVelocity(
+                () -> -m_driverController.getLeftY() * kDrive.MAX_DRIVE_VELOCIY,
+                () -> -m_driverController.getLeftX() * kDrive.MAX_DRIVE_VELOCIY,
+                () -> (m_driverController.getLeftTriggerAxis() - m_driverController.getRightTriggerAxis())
+                        * kDrive.MAX_TURN_ANGULAR_VELOCITY);
+
+        sys_drivetrain.setDefaultCommand(cmd_teleopDrive);
+
         m_driverController.y().onTrue(new DriveToNote());
     }
 
